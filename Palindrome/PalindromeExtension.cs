594a9a960070db62f8119bd6de5ebe646d0b11d2
Joe Taylor <jptaylor226@gmail.com>
2019-04-17 16:09:15 -0500

    Added palindromes

---
+++ Palindrome/PalindromeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PalindromeNS
+{
+    public static class PalindromeExtension
+    {
+        public static bool IsPalindrome(this string value, bool caseSensitive = false)
+        {
+            if (value == null) return false;
+            var result = true;
+            var normalized = Regex.Replace(value, @"[^\w]", string.Empty);
+            var characters = caseSensitive ? normalized.ToCharArray() : normalized.ToUpper().ToCharArray();
+            for(var i = 0; result && i < characters.Length/2; i++)
+            {
+                result = characters[i] == characters[characters.Length - i - 1];
+            }
+            return result;
+        }
+    }
+}
---
+++ XUnitSampleTests/PalindromeTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using PalindromeNS;
+using Xunit;
+
+namespace XUnitSampleTests
+{
+    public sealed class PalindromeTest
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("RADAR")]
+        [InlineData("level")]
+        [InlineData("madam")]
+        [InlineData("Madam, I'm Adam")]
+        [InlineData("Able was I, 'ere I saw Elba")]
+        [InlineData("Go hang a salami, I'm a lasagna hog!")]
+        public void IdentifyPalindromes(string candidate)
+        {
+            // setup
+            // execute
+            var result = candidate.IsPalindrome();
+            // assert
+            result.Should().BeTrue();
+        }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("levl")]
+        [InlineData("mada")]
+        [InlineData("Madam, I Adam")]
+        [InlineData("Able was I, 'eR I saw Elba")]
+        [InlineData("Go hang the sala, I'm a lasgna hog")]
+        public void IdentifyNotPalindromes(string candidate)
+        {
+            // setup
+            // execute
+            var result = candidate.IsPalindrome();
+            // assert
+            result.Should().BeFalse();
+        }
+        [Theory]
+        [InlineData("levEl", false)]
+        [InlineData("RADAR", true)]
+        [InlineData("madam", true)]
+        [InlineData("Madam, I'm adaM", true)]
+        [InlineData("Able was I, 'ere I saw Elba", false)]
+        public void IdentifyCaseSensitivePalindromes(string candidate, bool expected)
+        {
+            // setup
+            // execute
+            var result = candidate.IsPalindrome(true);
+            // assert
+            result.Should().Be(expected);
+        }
+    }
+}
