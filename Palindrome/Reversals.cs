175da3d6d6fd1a262a6ba0f314c6929253701fb1
Joe Taylor <joe.taylor@myresman.com>
2019-04-18 20:22:02 -0500

    Basic reversals

---
+++ Palindrome/Reversals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringExtensions
+{
+    public static class Reversals
+    {
+        public static string Reverse(this string value)
+        {
+            if (value == null) return value;
+            var characters = value.ToCharArray().Reverse();
+            return new string(characters);
+        }
+        public static string ReverseSentence(this string value)
+        {
+            if (value == null) return value;
+            var words = value.Split(' ');
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendJoin(' ', words.Reverse());
+            return stringBuilder.ToString();
+        }
+        public static T[] Reverse<T>(this T[] values)
+        {
+            if (values == null) return null;
+            var result = new T[values.Length];
+            values.CopyTo(result, 0);
+            var end = result.Length - 1;
+            for (var i = 0; i < end; i++, end--)
+            {
+                var temp = result[i];
+                result[i] = result[end];
+                result[end] = temp;
+            }
+            return result;
+        }
+    }
+}
---
+++ XUnitSampleTests/ReversalTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using StringExtensions;
+using System;
+using Xunit;
+
+namespace XUnitSampleTests
+{
+    public sealed class ReversalTest
+    {
+        [Theory]
+        [InlineData("abcdef", "fedcba")]
+        [InlineData("how do you do?", "?od uoy od woh")]
+        [InlineData(null, null)]
+        [InlineData("x", "x")]
+        public void ReverseStringTest(string input, string output)
+        {
+            // setup
+            // execute
+            var result = input.Reverse();
+            // aseert
+            result.Should().Be(output);
+        }
+        [Theory]
+        [InlineData("hello world", "world hello")]
+        [InlineData("how do you do?", "do? you do how")]
+        [InlineData(null, null)]
+        [InlineData("x", "x")]
+        public void ReverseSentenceTest(string input, string output)
+        {
+            // setup
+            // execute
+            var result = input.ReverseSentence();
+            // aseert
+            result.Should().Be(output);
+
+        }
+    }
+}
